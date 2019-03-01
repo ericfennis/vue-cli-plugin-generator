10b86566de7efa5e40007d4aaa8bd47966e8a2be
Eric Fennis <eric.fennis@gmail.com>
2019-03-01 16:07:15 +0100

    Removed unused code

--- generator/index.js
+++ generator/index.js
@@ -1,12 +1,3 @@
-const _ = require('lodash/string');
-
-const getNamings = string => ({
- value: string,
- camelCase: _.camelCase(string),
- pascalCase: _.upperFirst(_.camelCase(string)),
- kebabCase: _.kebabCase(string),
-})
-
 module.exports = (api, options) => {
 
   const packageJson = require(api.resolve('package.json'));
