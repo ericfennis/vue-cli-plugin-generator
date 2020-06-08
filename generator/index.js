511586d9f4b904b17de8ef54572d3b607903c778
Eric Fennis <eric.fennis@gmail.com>
2020-06-08 20:45:20 +0200

    remove console.log

--- generator/index.js
+++ generator/index.js
@@ -3,8 +3,6 @@ module.exports = (api, options) => {
   const packageJson = require(api.resolve('package.json'));
 
   if (!packageJson.scripts.hasOwnProperty('make')) {
-    console.log(packageJson);
-    
     require('./init')(api, options)
   } else {
     require('./generator')(api, options)
